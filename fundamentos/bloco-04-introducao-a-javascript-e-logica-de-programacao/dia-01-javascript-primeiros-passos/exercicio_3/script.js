7efea15a050981614cd7811f23e284f6c77c52b4
Yago Castro <yago.fanstour@gmail.com>
2022-08-18 15:25:25 -0300

    Exercicios do conteúdo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio_3/script.js
@@ -0,0 +1,13 @@
+const notaCandidato = 85;
+
+
+if (notaCandidato >= 80) {
+    console.log('Parabéns, você foi aprovado')
+}
+else if(notaCandidato < 80 && notaCandidato >= 60){
+    console.log('Você está na nossa lista de espera')
+}
+
+else {
+    console.log('Você foi reprovado')
+}
