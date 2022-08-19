0a2a116e6f14b6473a1c5e051810d5a571a69f0d
Yago Castro <yago.fanstour@gmail.com>
2022-08-19 18:19:01 -0300

    Exercicios do dia 4.2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicios/script9.js
@@ -0,0 +1,19 @@
+let numeros = [];
+let divide = 0;
+
+for (let i = 0; i <= 25; i +=1){
+    numeros.push(i)
+}
+console.log(numeros)
+
+for (let i = 0; i < numeros.length; i +=1){
+    divide = numeros[i]/2
+    console.log(divide)
+}
+
+
+
+
+
+
+
