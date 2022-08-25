77a4e31fe8d1cfa3daa9d20888153c3c6a029735
Yago Castro <yago.fanstour@gmail.com>
2022-08-25 19:26:45 -0300

    exercicios do dia 5.1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-dom-e-seletores/exercicios-do-conteudo/exercicio-2/script.js
@@ -0,0 +1,12 @@
+let head = document.getElementById('header-container');
+let title1 = document.getElementsByClassName('emergency-tasks')
+let aga3 = document.getElementsByTagName('h3')
+let title2 = document.querySelector('.no-emergency-tasks')
+
+head.style.backgroundColor = 'green'
+title1[0].style.backgroundColor = 'salmon'
+title2.style.background = 'yellow'
+aga3[0].style.background = 'purple'
+aga3[1].style.background = 'purple'
+aga3[2].style.background = 'black'
+aga3[3].style.background = 'black'
