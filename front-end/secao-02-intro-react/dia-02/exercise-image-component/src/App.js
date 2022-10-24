91846d74c697e644afa6e8d0b518a259bc505213
Yago Castro <yago.fanstour@gmail.com>
2022-10-24 15:02:20 -0300

    exercicios do dia 2

---
+++ front-end/secao-02-intro-react/dia-02/exercise-image-component/src/App.js
@@ -0,0 +1,10 @@
+import Image from './Image';
+import './App.css';
+
+function App() {
+  return (
+    <Image source = 'cat.jpg' alternativeText = "Cute cat staring"/>
+  );
+}
+
+export default App;
